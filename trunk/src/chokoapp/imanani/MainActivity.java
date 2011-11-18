457ba365859dc24f50861eb7692dc819b9fd713a
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2011-11-18 19:34:17 +0000

    レコーディング画面の見た目だけ作成 個別の機能については別途 Issue に挙げる
    Fixes Issue 5

--- trunk/src/chokoapp/imanani/MainActivity.java
+++ trunk/src/chokoapp/imanani/MainActivity.java
@@ -2,12 +2,15 @@ package chokoapp.imanani;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
     }
 }

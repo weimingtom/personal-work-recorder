1a94ba787cf3b3159321aacb907f7e1230a24938
mizzimax@gmail.com <mizzimax@gmail.com@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2011-12-01 16:27:25 +0000

    タスクを切り替えると作業時間が０から始まるようにしたよ。 トータル時間も随時更新されるようにしたよ。 Chronometerを使うのをやめて、HH:MM:SS形式にしたよ。 fixes issue 26

--- trunk/src/chokoapp/imanani/MainActivity.java
+++ trunk/src/chokoapp/imanani/MainActivity.java
@@ -9,17 +9,19 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.Chronometer;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private SQLiteDatabase db;
-    
+    private TimeKeeper timeKeeper;
+    private Ticker ticker;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,21 @@ public class MainActivity extends Activity {
         displayToday();
         db = (new DBOpenHelper(this)).getReadableDatabase();
         setupSpinner();
+
+        timeKeeper = new TimeKeeper();
+        ticker = new Ticker(timeKeeper);
+    }
+
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	ticker.start();
+    }
+
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	ticker.stop();
     }
 
     private void displayToday() {
@@ -56,16 +73,107 @@ public class MainActivity extends Activity {
                                                             android.R.layout.simple_spinner_item, tasks);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+		OnItemSelectedListener listener = new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				timeKeeper.changeTask();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				timeKeeper.changeTask();
+			}
+		};
+		spinner.setOnItemSelectedListener(listener);
     }
 
     public void onClickStartButton(View view) {
-        Chronometer chronometer = (Chronometer)findViewById(R.id.durationView);
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
+    	timeKeeper.beginWork();
     }
 
     public void onClickFinishButton(View view) {
-        Chronometer chronometer = (Chronometer)findViewById(R.id.durationView);
-        chronometer.stop();
+    	timeKeeper.endWork();
+    }
+
+    /**
+     * 勤務時間と作業時間を計って、表示に反映させるクラス。
+     */
+    private class TimeKeeper implements Runnable {
+
+        private long workStartTime;
+        private long taskStartTime;
+
+        /**
+         * 勤務開始。
+         * @return 通常は０を返す。勤務を終了せずに再び開始したら、前回開始してから現在までの時間を返す。
+         */
+        public long beginWork() {
+			long now = System.currentTimeMillis();
+			try {
+				return workStartTime == 0 ? 0 : now - workStartTime;
+			} finally {
+				workStartTime = taskStartTime = now;
+        	}
+		}
+
+        /**
+         * 勤務終了。
+         * @return 勤務開始から現在までの時間を返す。
+         */
+        public long endWork() {
+        	long now = System.currentTimeMillis();
+        	try {
+				return workStartTime == 0 ? 0 : now - workStartTime;
+        	} finally {
+        		workStartTime = taskStartTime = 0;
+        	}
+        }
+
+        /**
+         * 作業変更。
+         * @return 勤務開始または前回の作業変更から現在までの時間を返す。
+         */
+        public long changeTask() {
+			if (workStartTime == 0) {
+				// 勤務が開始していなければ、作業も開始しないで０を返す。
+				return 0;
+			}
+        	long now = System.currentTimeMillis();
+        	try {
+				return taskStartTime == 0 ? 0 : now - taskStartTime;
+        	} finally {
+        		taskStartTime = now;
+        	}
+        }
+
+        /**
+         * 勤務時間と作業時間を表示に反映させる。
+         */
+        @Override
+		public void run() {
+			long now = System.currentTimeMillis();
+			if (workStartTime != 0) {
+				TextView total = (TextView) findViewById(R.id.totalTimeView);
+				total.setText(format(now - workStartTime));
+			}
+			if (taskStartTime != 0) {
+				TextView duration = (TextView) findViewById(R.id.durationView);
+				duration.setText(format(now - taskStartTime));
+			}
+		}
+
+        /**
+         * 時間のフォーマッティング。
+         * @param time ミリ秒単位の時間。
+         * @return H:MM:SS形式の文字列。
+         */
+		public String format(long time) {
+			long sec = time / 1000;
+			long min = sec / 60;
+			long hour = min / 60;
+			return String.format("%02d:%02d:%02d", hour, min % 60, sec % 60);
+		}
+
     }
 }

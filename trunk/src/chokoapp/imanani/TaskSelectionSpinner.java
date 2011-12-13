885028ce14b25e294bea80da59f18cf584f42bef
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2011-12-13 09:46:35 +0000

    現在選択されているタスクがアプリケーションを終了しても保持されるよう修正
    Spinner を拡張して、現在選択されているタスクID(テーブルのキー)を保持するクラスを作成し
    レイアウトファイル(XML)を使用せずに、MainActivityで自前でaddViewするよう変更
    
    現在選択されているタスクIDはSharedPreferencesに保存するようにした。
    保存のタイミングが onStart() / onStop() でいいのかは不明
    
    Fixes Issue 43

---
+++ trunk/src/chokoapp/imanani/TaskSelectionSpinner.java
@@ -0,0 +1,54 @@
+package chokoapp.imanani;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class TaskSelectionSpinner extends Spinner {
+    private long currentTaskId;
+    private TimeKeeper timeKeeper;
+
+    public TaskSelectionSpinner(Context context, ArrayAdapter<Task> adapter, 
+                                long currentTaskId, TimeKeeper timeKeeper) {
+        super(context);
+        this.currentTaskId = currentTaskId;
+        this.timeKeeper = timeKeeper;
+
+        setPromptId(R.string.selectTask);
+        setAdapter(adapter);
+        int spinnerPosition = 0;
+        int count = adapter.getCount();
+        for (int i = 0 ; i < count ; i++ ) {
+            if (adapter.getItem(i).getId() == currentTaskId) {
+                spinnerPosition = i;
+            }
+        }
+        setSelection(spinnerPosition);
+        setOnItemSelectedListener(new ChangeTask(spinnerPosition));
+    }
+
+    public long getCurrentTaskId() { return currentTaskId; }
+
+    private class ChangeTask implements OnItemSelectedListener {
+        private int currentSpinnerPosition;
+        public ChangeTask(int currentSpinnerPosition) {
+            this.currentSpinnerPosition = currentSpinnerPosition;
+        }
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            if ( currentSpinnerPosition != pos ) {
+                Task selectedTask = (Task)parent.getItemAtPosition(pos);
+                currentTaskId = selectedTask.getId();
+                timeKeeper.changeTask(selectedTask);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+        }
+    }
+}

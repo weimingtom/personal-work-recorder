ab5bc7819bfd1d1ef4f2ca338b0881e55aa39cb8
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2011-12-31 02:15:38 +0000

    プラス／マイナスボタンの長押しを実装
    長押しすると連続して開始時刻や終了時刻が上下する
    
    Fixes Issue 46

---
+++ src/chokoapp/imanani/UpButton.java
@@ -0,0 +1,79 @@
+package chokoapp.imanani;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.view.MotionEvent;
+
+public class UpButton extends ImageView {
+    private ContinuouslyUp upper;
+    private DateTimeView targetView;
+
+    public UpButton(Context context) {
+        this(context, null);
+    }
+
+    public UpButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setClickable(true);
+        setImageResource(R.drawable.plus_button);
+    }
+
+    public void setupListeners(DateTimeView view) {
+        this.targetView = view;
+        setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    targetView.up();
+                }
+            });
+
+        setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    upper = new ContinuouslyUp();
+                    upper.execute();
+                    return true;
+                }
+            });
+        setOnTouchListener(new OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch (event.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        if ( upper != null) upper.cancel(true);
+                    case MotionEvent.ACTION_DOWN:
+                        break;
+                    default:
+                        break;
+                    }
+                    return false;
+                }
+            });
+    }
+
+    private class ContinuouslyUp extends AsyncTask<Void, Void, Void> {
+        private static final int UPDATE_INTERVAL = 500;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while ( true ) {
+                if ( isCancelled() ) {
+                    return null;
+                }
+                try {
+                    Thread.sleep(UPDATE_INTERVAL);
+                } catch (InterruptedException e) {
+                    return null;
+                }
+                publishProgress();
+            }
+        }
+
+        protected void onProgressUpdate(Void... values) {
+            targetView.up();
+        }
+    }
+}

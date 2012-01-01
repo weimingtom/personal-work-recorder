c9bd9ce40919878a1dfa8b1b976090598cf8f652
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-01-01 07:20:30 +0000

    UpButton, DownButton で setupListeners() の引数を抽象型に修正

--- src/chokoapp/imanani/DownButton.java
+++ src/chokoapp/imanani/DownButton.java
@@ -21,7 +21,7 @@ public class DownButton extends ImageView {
         setImageResource(R.drawable.minus_button);
     }
 
-    public void setupListeners(DateTimeView view) {
+    public void setupListeners(UpDownView view) {
         this.targetView = view;
         setOnClickListener(new OnClickListener() {
                 @Override

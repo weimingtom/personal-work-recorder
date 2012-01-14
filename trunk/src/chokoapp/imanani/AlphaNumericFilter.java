c25a9b852c37019ea226890d8945c5ddf2e401b0
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-01-14 23:50:57 +0000

    作業コードには英数字のみ入力可能に制限
    Fixes Issue 89
    
    英数字以外が入力された場合はトーストでメッセージを表示することとしたが、
    ちょっと表示されすぎる。
    
    日本語入力で「あ」と入力 → トースト表示 → 入力モードを「A」に変更
    で再びトーストが出てしまう。
    
    どうも「あ」と入力した時の編集結果自体はクリアされているが、「'あ'を変換中」とい
    う情報は残っていて、「A」に切り替えた時にその入力中文字を確定しようとして再びフィ
    ルターが動いてるみたい。「変換中の文字列」をクリアする方法がわからない。

--- trunk/src/chokoapp/imanani/AlphaNumericFilter.java
+++ trunk/src/chokoapp/imanani/AlphaNumericFilter.java
@@ -1,12 +1,26 @@
 package chokoapp.imanani;
 
+import android.content.Context;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.widget.Toast;
 
 public class AlphaNumericFilter implements InputFilter {
+    private Context context;
+
+    public AlphaNumericFilter(Context context) {
+        this.context = context;
+    }
+
     @Override
     public CharSequence filter(CharSequence source, int start, int end,
                                Spanned dest, int dstart, int dend) {
-        return source.toString().matches("^[a-zA-Z0-9]+$") ? source : "";
+        if ( source.toString().matches("^[a-zA-Z0-9]*$") ) {
+            return null;
+        } else {
+            Toast.makeText(context, context.getString(R.string.should_be_alpha_numeric),
+                           Toast.LENGTH_SHORT).show();
+            return  "";
+        }
     }
 }

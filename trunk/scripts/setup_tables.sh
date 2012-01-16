b7f7947e8e78b85989248b4c1592540f2fcbf530
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-01-16 14:20:46 +0000

    SQLite の date 関数の使い方を訂正
    Fixes Issue 100

--- trunk/scripts/setup_tables.sh
+++ trunk/scripts/setup_tables.sh
@@ -18,13 +18,13 @@ EOF
 
 sqlite3 pwr.db <<EOF
 INSERT INTO work_records (_id, start_time, end_time)
-       VALUES (1, strftime('%s', date('now', '-3days')||' 09:00:00', 'utc') * 1000,
-                  strftime('%s', date('now', '-3days')||' 18:30:00', 'utc') * 1000);
+       VALUES (1, strftime('%s', date('now', '-3 days')||' 09:00:00', 'utc') * 1000,
+                  strftime('%s', date('now', '-3 days')||' 18:30:00', 'utc') * 1000);
 INSERT INTO work_records (_id, start_time, end_time)
-       VALUES (2, strftime('%s', date('now', '-2days')||' 10:00:00', 'utc') * 1000,
-                  strftime('%s', date('now', '-2days')||' 22:30:00', 'utc') * 1000);
+       VALUES (2, strftime('%s', date('now', '-2 days')||' 10:00:00', 'utc') * 1000,
+                  strftime('%s', date('now', '-2 days')||' 22:30:00', 'utc') * 1000);
 INSERT INTO work_records (_id, start_time, end_time)
-       VALUES (3, strftime('%s', date('now', '-1days')||' 11:00:00', 'utc') * 1000,
+       VALUES (3, strftime('%s', date('now', '-1 days')||' 11:00:00', 'utc') * 1000,
                   strftime('%s', date('now')||' 02:00:00', 'utc') * 1000);
 INSERT INTO work_records (_id, start_time, end_time)
        VALUES (4, strftime('%s', date('now')||' 10:00:00', 'utc') * 1000, NULL);
@@ -32,41 +32,41 @@ EOF
 
 sqlite3 pwr.db <<EOF
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 09:00:00', 'utc') * 1000, 'A01', 'ミーティング');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 09:00:00', 'utc') * 1000, 'A01', 'ミーティング');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 11:00:00', 'utc') * 1000, 'A02', '進捗報告会');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 11:00:00', 'utc') * 1000, 'A02', '進捗報告会');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 13:00:00', 'utc') * 1000, 'B03', 'テスト');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 13:00:00', 'utc') * 1000, 'B03', 'テスト');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 17:00:00', 'utc') * 1000, 'D01', '事務作業');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 17:00:00', 'utc') * 1000, 'D01', '事務作業');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (1, strftime('%s', date('now', '-3days')||' 18:00:00', 'utc') * 1000, 'B01', '詳細設計');
+        VALUES (1, strftime('%s', date('now', '-3 days')||' 18:00:00', 'utc') * 1000, 'B01', '詳細設計');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 10:00:00', 'utc') * 1000, 'A01', 'ミーティング');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 10:00:00', 'utc') * 1000, 'A01', 'ミーティング');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 10:30:00', 'utc') * 1000, 'D01', '事務作業');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 10:30:00', 'utc') * 1000, 'D01', '事務作業');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 11:00:00', 'utc') * 1000, 'B01', '詳細設計');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 11:00:00', 'utc') * 1000, 'B01', '詳細設計');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 13:00:00', 'utc') * 1000, 'B01', '詳細設計');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 13:00:00', 'utc') * 1000, 'B01', '詳細設計');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 17:00:00', 'utc') * 1000, 'A01', 'ミーティング');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 17:00:00', 'utc') * 1000, 'A01', 'ミーティング');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (2, strftime('%s', date('now', '-2days')||' 18:00:00', 'utc') * 1000, 'F01', '本番リリース作業');
+        VALUES (2, strftime('%s', date('now', '-2 days')||' 18:00:00', 'utc') * 1000, 'F01', '本番リリース作業');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (3, strftime('%s', date('now', '-1days')||' 11:00:00', 'utc') * 1000, 'A01', 'ミーティング');
+        VALUES (3, strftime('%s', date('now', '-1 days')||' 11:00:00', 'utc') * 1000, 'A01', 'ミーティング');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (3, strftime('%s', date('now', '-1days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
+        VALUES (3, strftime('%s', date('now', '-1 days')||' 12:00:00', 'utc') * 1000, 'C01', '休憩');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (3, strftime('%s', date('now', '-1days')||' 13:00:00', 'utc') * 1000, 'A02', '進捗報告会');
+        VALUES (3, strftime('%s', date('now', '-1 days')||' 13:00:00', 'utc') * 1000, 'A02', '進捗報告会');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (3, strftime('%s', date('now', '-1days')||' 15:00:00', 'utc') * 1000, 'B03', 'テスト');
+        VALUES (3, strftime('%s', date('now', '-1 days')||' 15:00:00', 'utc') * 1000, 'B03', 'テスト');
 INSERT INTO task_records (work_id, start_time, code, description)
-        VALUES (3, strftime('%s', date('now', '-1days')||' 19:00:00', 'utc') * 1000, 'F02', '本番リリース作業');
+        VALUES (3, strftime('%s', date('now', '-1 days')||' 19:00:00', 'utc') * 1000, 'F02', '本番リリース作業');
 INSERT INTO task_records (work_id, start_time, code, description)
         VALUES (4, strftime('%s', date('now')||' 10:00:00', 'utc') * 1000, 'A01', 'ミーティング');
 INSERT INTO task_records (work_id, start_time, code, description)

67b9c6012ce91f805d4ef7cc01a0c7b3c8c07f56
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-02-24 21:48:40 +0000

    CalendarView を作成中
    見た目だけ作成。これからすること
    
    ・年月を選択したら、表示されるカレンダーを切り替える
    ・年月を選択した時の Listener を追加
    ・日を選択した時の Listener を追加
    ・日のマスにその日の作業時間を時分で表示
    ・日のマスにその日の作業記録はあっても調整していない場合を表示
    ・合計時間にその月の合計時間を表示

---
+++ src/chokoapp/imanani/CalendarDateView.java
@@ -0,0 +1,42 @@
+package chokoapp.imanani;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class CalendarDateView extends RelativeLayout {
+    private LayoutInflater inf;
+    private TextView dateView;
+    private TextView timeView;
+
+    public CalendarDateView(Context context) {
+        this(context, null, 0);
+    }
+    public CalendarDateView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+    public CalendarDateView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        inf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View topLayout = inf.inflate(R.layout.date_cell, this);
+
+        dateView = (TextView)topLayout.findViewById(R.id.calendarDateView);
+        timeView = (TextView)topLayout.findViewById(R.id.calendarDayTotalView);
+    }
+
+    public void setBackground(boolean isSunday) {
+        setBackgroundResource(isSunday ? R.drawable.date_cell_sunday : R.drawable.date_cell);
+    }
+
+    public void setDate(int date) {
+        dateView.setText(Integer.toString(date));
+    }
+
+    public void setInvalid() {
+        setBackgroundResource(R.drawable.date_cell_invalid);
+        timeView.setText("");
+    }
+}
---
+++ src/chokoapp/imanani/DateInfo.java
@@ -0,0 +1,67 @@
+package chokoapp.imanani;
+
+import java.util.Calendar;
+
+public class DateInfo {
+    private static long MILLISEC_OF_DAY = 24 * 60 * 60 * 1000;
+    private Calendar date;
+    private int baseYear;
+    private int baseMonth;
+
+    public DateInfo(int year, int month) {
+        baseYear = year;
+        baseMonth = month;
+        this.date = Calendar.getInstance();
+        date.clear();
+        date.set(year, month - 1, 1);
+    }
+    public int getDate() {
+        return date.get(Calendar.DATE);
+    }
+    public int getMonth() {
+        return date.get(Calendar.MONTH) + 1;
+    }
+    public int getYear() {
+        return date.get(Calendar.YEAR);
+    }
+    public boolean isSunday() {
+        return date.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+    }
+    public void moveTopCorner() {
+        date.set(baseYear, baseMonth - 1, 1);
+        while (date.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
+            date.add(Calendar.DATE, -1);
+        }
+    }
+    public boolean isBottomCorner() {
+        Calendar temp = Calendar.getInstance();
+        temp.set(baseYear, baseMonth - 1, 1);
+        temp.add(Calendar.MONTH, 1);
+        temp.add(Calendar.DATE, -1);
+        while (temp.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
+            temp.add(Calendar.DATE, 1);
+        }
+        return date.get(Calendar.YEAR)  == temp.get(Calendar.YEAR) &&
+               date.get(Calendar.MONTH) == temp.get(Calendar.MONTH) &&
+               date.get(Calendar.DATE)  == temp.get(Calendar.DATE);
+    }
+    public boolean moveToNext() {
+        if ( isBottomCorner() ) return false;
+        date.add(Calendar.DATE, 1);
+        return true;
+    }
+    public int daysFrom(Calendar from) {
+        long d = date.getTime().getTime() - from.getTime().getTime();
+        return (int)(d / MILLISEC_OF_DAY);
+    }
+    public int getRow() {
+        DateInfo topCorner = new DateInfo(baseYear, baseMonth);
+        topCorner.moveTopCorner();
+        return daysFrom(topCorner.date) / 7;
+    }
+    public int getColumn() {
+        DateInfo topCorner = new DateInfo(baseYear, baseMonth);
+        topCorner.moveTopCorner();
+        return daysFrom(topCorner.date) % 7;
+    }
+}

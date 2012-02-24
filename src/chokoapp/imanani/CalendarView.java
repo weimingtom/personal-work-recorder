13c49c0bcada6e2c2a87253d2459e4cedc5b9889
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-02-24 21:49:07 +0000

    年月の辺りをクリックしたらカレンダーを切り替える
    利用側で、setOnMonthSelectListener を呼び出すことで、
    カレンダーを切り替えた時の動作をコールバックとして登録
    できるようにもした。

--- src/chokoapp/imanani/CalendarView.java
+++ src/chokoapp/imanani/CalendarView.java
@@ -1,25 +1,34 @@
 package chokoapp.imanani;
 
 import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 
 public class CalendarView extends LinearLayout {
     private LayoutInflater inf;
-    private TableRow[] tableRows = new TableRow[6];
+    private TableRow[] weekRows = new TableRow[6];
     private CalendarDateView[][] calendarDates = new CalendarDateView[6][7];
     private OnMonthSelectListener listener;
-    private View topLayout;
+    private LinearLayout monthSelectLayout;
+    private int selectedYear;
+    private int selectedMonth; /* 1: January, 2: February, 3: March ... */
+    private TextView monthSelectView;
 
     public interface OnMonthSelectListener {
         public void onSelectMonth(int year, int month);
@@ -31,22 +40,27 @@ public class CalendarView extends LinearLayout {
     public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         inf = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        topLayout = inf.inflate(R.layout.calendar, this);
+        View topLayout = inf.inflate(R.layout.calendar, this);
 
-        tableRows[0] = (TableRow)topLayout.findViewById(R.id.firstWeekRow);
-        tableRows[1] = (TableRow)topLayout.findViewById(R.id.secondWeekRow);
-        tableRows[2] = (TableRow)topLayout.findViewById(R.id.thirdWeekRow);
-        tableRows[3] = (TableRow)topLayout.findViewById(R.id.fourthWeekRow);
-        tableRows[4] = (TableRow)topLayout.findViewById(R.id.fifthWeekRow);
-        tableRows[5] = (TableRow)topLayout.findViewById(R.id.sixthWeekRow);
+        monthSelectView = (TextView)topLayout.findViewById(R.id.monthSelectView);
 
         Field[] idFields = R.id.class.getFields();
         for (Field field : idFields) {
+            Pattern weekRowPattern = Pattern.compile("weekRow(\\d)");
+            Matcher matchRow = weekRowPattern.matcher(field.getName());
             Pattern calendarDatePattern = Pattern.compile("calendarDate(\\d)(\\d)");
-            Matcher match = calendarDatePattern.matcher(field.getName());
-            if (match.matches()) {
-                int row = Integer.parseInt(match.group(1));
-                int col = Integer.parseInt(match.group(2));
+            Matcher matchDate = calendarDatePattern.matcher(field.getName());
+            if (matchRow.matches()) {
+                int row = Integer.parseInt(matchRow.group(1));
+                try {
+                    weekRows[row] = (TableRow)topLayout.findViewById(field.getInt(null));
+                } catch (IllegalArgumentException e) {e.printStackTrace();
+                } catch (IllegalAccessException e) {e.printStackTrace();
+                }
+            }
+            if (matchDate.matches()) {
+                int row = Integer.parseInt(matchDate.group(1));
+                int col = Integer.parseInt(matchDate.group(2));
                 try {
                     calendarDates[row][col] = (CalendarDateView)topLayout.findViewById(field.getInt(null));
                 } catch (IllegalArgumentException e) {e.printStackTrace();
@@ -55,10 +69,22 @@ public class CalendarView extends LinearLayout {
             }
         }
         Calendar now = Calendar.getInstance();
-        setMonth(now.get(Calendar.YEAR), now.get(Calendar.MONTH)+1);
+        selectedYear = now.get(Calendar.YEAR);
+        selectedMonth = now.get(Calendar.MONTH)+1;
+        setMonth(selectedYear, selectedMonth);
+
+        monthSelectLayout = (LinearLayout)topLayout.findViewById(R.id.monthSelectLayout);
+        monthSelectLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    showMonthPicker(selectedYear, selectedMonth);
+                }
+            });
     }
 
     public void setMonth(int year, int month) {
+        setMonthTitle(year, month);
+
         TableLayout.LayoutParams param = new TableLayout.LayoutParams();
         param.weight = 1;
 
@@ -75,9 +101,42 @@ public class CalendarView extends LinearLayout {
             }
         } while (dateInfo.moveToNext());
         for ( int i = dateInfo.getRow() + 1 ; i < 6 ; i++ ) {
-            removeView(tableRows[i]);
+            removeView(weekRows[i]);
         }
     }
 
     public void setOnMonthSelectListener(OnMonthSelectListener l) { listener = l; }
+
+    private void showMonthPicker(int year, int month) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        final DatePicker datePicker = new DatePicker(getContext());
+        datePicker.updateDate(year, month-1, 1);
+        int day_id = Resources.getSystem().getIdentifier("day", "id", "android");
+        datePicker.findViewById(day_id).setVisibility(View.GONE);
+        builder.setView(datePicker)
+            .setTitle(getContext().getString(R.string.select_month))
+            .setPositiveButton(android.R.string.ok,
+                               new DialogInterface.OnClickListener() {
+                                   @Override
+                                   public void onClick(DialogInterface d, int w) {
+                                       selectedYear = datePicker.getYear();
+                                       selectedMonth = datePicker.getMonth()+1;
+                                       setMonth(selectedYear, selectedMonth);
+                                       if (listener != null ) {
+                                           listener.onSelectMonth(selectedYear, selectedMonth);
+                                       }
+                                   }
+                               })
+            .setNegativeButton(android.R.string.cancel, null)
+            .create()
+            .show();
+    }
+
+    private void setMonthTitle(int year, int month) {
+        Calendar now = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy年MM月");
+        now.set(Calendar.YEAR, year);
+        now.set(Calendar.MONTH, month-1);
+        monthSelectView.setText(df.format(now.getTime()));
+   }
 }

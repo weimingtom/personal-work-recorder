d8dbf1081d165f425b80540a783a21a5017a4475
saeki.endo <saeki.endo@gmail.com>
2012-02-26 00:24:16 +0000

    Fixes Issue 121 Fixes Issue 122
    設定画面の1日の作業時間の入力フィールドに対して以下を修正
    　・既に設定済みの値がある場合は、その値をSummaryに表示
    　・ブランク入力しても、異常終了しないように修正
    　・値が不正な場合は、Toastにて注意を喚起するように修正
    ・設定画面の

--- src/chokoapp/imanani/AppPreferenceActivity.java
+++ src/chokoapp/imanani/AppPreferenceActivity.java
@@ -2,10 +2,12 @@ package chokoapp.imanani;
 
 import android.os.Bundle;
 import android.preference.EditTextPreference;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.view.Gravity;
+import android.widget.Toast;
 
 public class AppPreferenceActivity extends PreferenceActivity {
     @Override
@@ -14,17 +16,37 @@ public class AppPreferenceActivity extends PreferenceActivity {
         addPreferencesFromResource(R.xml.preferences);
 
         EditTextPreference etp = (EditTextPreference)findPreference("monthly_summary_time_of_day");
+        String monthlySummaryTimeOfDay = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString("monthly_summary_time_of_day","");
+        if (monthlySummaryTimeOfDay != "") {
+            etp.setSummary(monthlySummaryTimeOfDay + getString(R.string.hours));
+        }
         etp.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
-                String input = newValue.toString();
-                if (input != null && Integer.parseInt(input) > 0 && Integer.parseInt(input) <= 24) {
+
+                int inputValue;
+                try {
+                    inputValue = Integer.parseInt((String)newValue);
+                }
+                catch (NumberFormatException e) {
+                    showToast(R.string.monthly_ummary_preference_time_of_day_input_msg);
+                    return false;
+                }
+                if (inputValue > 0 && inputValue <= 24) {
                     ((EditTextPreference)preference).setSummary((String)newValue + getString(R.string.hours));
                     return true;
                 } else {
+                    showToast(R.string.monthly_ummary_preference_time_of_day_input_msg);
                     return false;
                 }
             }
         });
+    }
+
+    private void showToast(int resId) {
+        Toast toast = Toast.makeText(AppPreferenceActivity.this, resId,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }

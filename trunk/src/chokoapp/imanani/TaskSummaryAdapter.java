b4d1ef9f373b83a47b1fb44f0819cf33d0c7a728
kozaki.tsuneaki <kozaki.tsuneaki@d2d7a146-6f43-ab4f-a6cb-50ca79005b2a>
2012-02-29 03:15:34 +0000

    TaskSummaryAdapter#isEmpty() はオーバーライドする必要がないので削除

--- trunk/src/chokoapp/imanani/TaskSummaryAdapter.java
+++ trunk/src/chokoapp/imanani/TaskSummaryAdapter.java
@@ -108,8 +108,6 @@ public class TaskSummaryAdapter extends ArrayAdapter<DailyTaskSummary> {
         return sum;
     }
 
-    public boolean isEmpty() { return getCount() == 0; }
-
     @SuppressWarnings("serial")
     public List<Task> getRemainedTasks(final SQLiteDatabase db) {
         return new ArrayList<Task>() {{
